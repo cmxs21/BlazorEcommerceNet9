84b9672bb8f161eacd018e6bd1fc1773e4d79091
cmxs21 <229416960+cmxs21@users.noreplay.github.com>
2025-09-22 13:28:36 -0600

    Section 12 - End - Categories Administration Completed

---
+++ BlazorEcommerce/wwwroot/js/common.js
@@ -0,0 +1,9 @@
+﻿function ModalConfirmationShow() {
+    var confirmationModal = new bootstrap.Modal(document.getElementById('ModalGeneralConfirm'));
+    confirmationModal.show();
+}
+function ModalConfirmationHide() {
+    //var confirmationModal = new bootstrap.Modal(document.getElementById('ModalGeneralConfirm'));
+    //confirmationModal.hide();
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('ModalGeneralConfirm')).hide();
+}
