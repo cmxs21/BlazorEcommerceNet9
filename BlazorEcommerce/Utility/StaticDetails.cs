6fab955acd7f723b9eef99ea2a57b5a2f8115d57
cmxs21 <229416960+cmxs21@users.noreply.github.com>
2025-09-27 11:06:19 -0600

    Section 15 - Display Category in Filter - Done

---
+++ BlazorEcommerce/Utility/StaticDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Utility
+{
+    public static class StaticDetails
+    {
+        public static string RoleAdmin = "Admin";
+        public static string RoleCustomer = "Customer";
+    }
+}
